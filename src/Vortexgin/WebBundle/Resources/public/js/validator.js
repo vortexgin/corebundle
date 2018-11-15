7f2f95b2c1747eb9681c5eb00d1b8943ca83fe6b
Tommy's Mac <vortexgin@gmail.com>
2018-11-15 15:42:47 +0700

    Changes Log: - add javascript escape for xss prevention

--- src/Vortexgin/WebBundle/Resources/public/js/validator.js
+++ src/Vortexgin/WebBundle/Resources/public/js/validator.js
@@ -30,5 +30,13 @@ var Validator = {
         validate: function (_var, _key, _is) {
 
         }
+    }, 
+    escape: function(toOutput){
+        return toOutput.replace(/\&/g, '&amp;')
+            .replace(/\</g, '&lt;')
+            .replace(/\>/g, '&gt;')
+            .replace(/\"/g, '&quot;')
+            .replace(/\'/g, '&#x27')
+            .replace(/\//g, '&#x2F');
     }
 };
