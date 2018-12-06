86293f181db088e9afe85048be4e21a4b3f942a6
Tommy's Mac <vortexgin@gmail.com>
2018-12-06 17:41:53 +0700

    Changes Log: - rollback encrypt

--- src/Vortexgin/LibraryBundle/Utils/Encrypt.php
+++ src/Vortexgin/LibraryBundle/Utils/Encrypt.php
@@ -22,13 +22,13 @@ class Encrypt
      * 
      * @return mixed
      */
-    public static function encode($string, $secret, $tag='vortexgin')
+    public static function encode($string, $secret)
     {
         $cipher = "aes-128-gcm";
         if (in_array($cipher, openssl_get_cipher_methods())) {
             $ivlen = openssl_cipher_iv_length($cipher);
             $iv = openssl_random_pseudo_bytes($ivlen);
-            return openssl_encrypt($string, $cipher, $secret, 0, $iv, $tag);
+            return openssl_encrypt($string, $cipher, $secret, 0, $iv);
         }
 
         return false;
@@ -42,13 +42,13 @@ class Encrypt
      * 
      * @return mixed
      */
-    public static function decode($string, $secret, $tag='vortexgin')
+    public static function decode($string, $secret)
     {
         $cipher = "aes-128-gcm";
         if (in_array($cipher, openssl_get_cipher_methods())) {
             $ivlen = openssl_cipher_iv_length($cipher);
             $iv = openssl_random_pseudo_bytes($ivlen);
-            return openssl_decrypt($string, $cipher, $secret, 0, $iv, $tag);
+            return openssl_decrypt($string, $cipher, $secret, 0, $iv);
         }
 
         return false;
