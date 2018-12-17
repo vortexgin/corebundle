40fec8d355262797bfa94b08551447749d1c3a14
Tommy Dian P <vortexgin@Tommys-MacBook-Pro.local>
2018-12-17 14:59:42 +0700

    Changes Log: - initiate js parse url

--- src/Vortexgin/WebBundle/Resources/public/js/url.js
+++ src/Vortexgin/WebBundle/Resources/public/js/url.js
@@ -4,18 +4,23 @@
  * Author Tommy Dian P
  */
 var URL = {
-    getQuery: function(name){
+    parse: function(url) {
+        var l = document.createElement("a");
+        l.href = url;
+        return l;
+    },
+    getQuery: function(name) {
         var url = location.search;
         name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
         var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
             results = regex.exec(url);
         return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
     },
-    getQueryToObject: function(){
-        var queries = document.location.search.replace(/(^\?)/,'').split("&").map(function(n){return n = n.split("="),this[n[0]] = n[1],this}.bind({}))[0], 
+    getQueryToObject: function() {
+        var queries = document.location.search.replace(/(^\?)/, '').split("&").map(function(n) { return n = n.split("="), this[n[0]] = n[1], this }.bind({}))[0],
             returns = {};
         Object.keys(queries).forEach(function(key) {
-            if(!empty(queries[key])){
+            if (!empty(queries[key])) {
                 returns[key] = queries[key];
             }
         });
