6b385e372ade3d99efa493231a9573fa36e43e06
Tommy Dian P <vortexgin@Tommys-MacBook-Pro.local>
2018-12-19 12:32:25 +0700

    Changes Log: - bugfix orm generate filter

--- src/Vortexgin/LibraryBundle/Utils/Doctrine/ORM/FilterGenerator.php
+++ src/Vortexgin/LibraryBundle/Utils/Doctrine/ORM/FilterGenerator.php
@@ -20,7 +20,6 @@ class FilterGenerator
      * Function to generate filter query
      *
      * @param Doctrine\ORM\QueryBuilder $queryBuilder Query Builder
-     * @param object                    $object       Object entity
      * @param array                     $param        Filter parameters
      *                                                - field
      *                                                - value
@@ -32,65 +31,63 @@ class FilterGenerator
      * 
      * @return Doctrine\ORM\QueryBuilder
      */
-    public static function generate(QueryBuilder $queryBuilder, $object, array $param)
+    public static function generate(QueryBuilder $queryBuilder, array $param)
     {
         if (is_array($param) && count($param) > 0) {
             $index = 0;
             foreach ($param as $key=>$value) {
                 if (!is_null($value[0]) && !is_null($value[1])) {
-                    if (in_array($value[0], $this->listSearchFields)) {
-                        $annotation = 'andWhere';
-                        $field = 'er.'.$value[0];
-                        $operator = array_key_exists(2, $value)?$value[2]:null;
+                    $annotation = 'andWhere';
+                    $field = 'er.'.$value[0];
+                    $operator = array_key_exists(2, $value)?$value[2]:null;
 
-                        if(array_key_exists(3, $value) && strtolower($value[3]) == 'or')
-                            $annotation = 'orWhere';
-                        if(array_key_exists(4, $value))
-                            $field = $value[4].'.'.$value[0];
-                        if (array_key_exists(5, $value)) {
-                            $function = $value[5];
-                            if (array_key_exists(1, $value[5])) {
-                                $field = "{$function[0]}({$field}, '{$function[1]}')";
-                            } else {
-                                $field = "{$function[0]}({$field})";
-                            }
+                    if(array_key_exists(3, $value) && strtolower($value[3]) == 'or')
+                        $annotation = 'orWhere';
+                    if(array_key_exists(4, $value))
+                        $field = $value[4].'.'.$value[0];
+                    if (array_key_exists(5, $value)) {
+                        $function = $value[5];
+                        if (array_key_exists(1, $value[5])) {
+                            $field = "{$function[0]}({$field}, '{$function[1]}')";
+                        } else {
+                            $field = "{$function[0]}({$field})";
                         }
+                    }
 
-                        switch(strtolower($operator)){
-                        case 'notnull' : 
-                            $queryBuilder->$annotation("{$field} IS NOT NULL");
-                            break;
-                        case 'null' : 
-                            $queryBuilder->$annotation("{$field} IS NULL");
-                            break;
-                        case 'notlike' : 
-                            $queryBuilder->$annotation("{$field} NOT LIKE '%{$value[1]}%'");
-                            break;
-                        case 'like' : 
-                            $queryBuilder->$annotation("{$field} LIKE '%{$value[1]}%'");
-                            break;
-                        case 'lt' : 
-                            $queryBuilder->$annotation("{$field} <= (:valueLt{$index})")->setParameter("valueLt{$index}", $value[1]);
-                            break;
-                        case 'gt' : 
-                            $queryBuilder->$annotation("{$field} >= (:valueGt{$index})")->setParameter("valueGt{$index}", $value[1]);
-                            break;
-                        case 'notin' :
-                            $queryBuilder->$annotation("{$field} NOT IN (:valueNotIn{$index})")->setParameter("valueNotIn{$index}", $value[1]);
-                            break;
-                        case 'in' :
-                            $queryBuilder->$annotation("{$field} IN (:valueIn{$index})")->setParameter("valueIn{$index}", $value[1]);
-                            break;
-                        case 'notequal' :
-                            $queryBuilder->$annotation("{$field} <> :valueNoEqual{$index}")->setParameter("valueNoEqual{$index}", $value[1]);
-                            break;
-                        case 'regexp' :
-                            $queryBuilder->$annotation("REGEXP({$field}, :valueRegexp{$index}) = true")->setParameter("valueRegexp{$index}", $value[1]);
-                            break;
-                        default:
-                            $queryBuilder->$annotation("{$field} = :valueEqual{$index}")->setParameter("valueEqual{$index}", $value[1]);
-                            break;
-                        }
+                    switch(strtolower($operator)){
+                    case 'notnull' : 
+                        $queryBuilder->$annotation("{$field} IS NOT NULL");
+                        break;
+                    case 'null' : 
+                        $queryBuilder->$annotation("{$field} IS NULL");
+                        break;
+                    case 'notlike' : 
+                        $queryBuilder->$annotation("{$field} NOT LIKE '%{$value[1]}%'");
+                        break;
+                    case 'like' : 
+                        $queryBuilder->$annotation("{$field} LIKE '%{$value[1]}%'");
+                        break;
+                    case 'lt' : 
+                        $queryBuilder->$annotation("{$field} <= (:valueLt{$index})")->setParameter("valueLt{$index}", $value[1]);
+                        break;
+                    case 'gt' : 
+                        $queryBuilder->$annotation("{$field} >= (:valueGt{$index})")->setParameter("valueGt{$index}", $value[1]);
+                        break;
+                    case 'notin' :
+                        $queryBuilder->$annotation("{$field} NOT IN (:valueNotIn{$index})")->setParameter("valueNotIn{$index}", $value[1]);
+                        break;
+                    case 'in' :
+                        $queryBuilder->$annotation("{$field} IN (:valueIn{$index})")->setParameter("valueIn{$index}", $value[1]);
+                        break;
+                    case 'notequal' :
+                        $queryBuilder->$annotation("{$field} <> :valueNoEqual{$index}")->setParameter("valueNoEqual{$index}", $value[1]);
+                        break;
+                    case 'regexp' :
+                        $queryBuilder->$annotation("REGEXP({$field}, :valueRegexp{$index}) = true")->setParameter("valueRegexp{$index}", $value[1]);
+                        break;
+                    default:
+                        $queryBuilder->$annotation("{$field} = :valueEqual{$index}")->setParameter("valueEqual{$index}", $value[1]);
+                        break;
                     }
                 }
                 $index++;
